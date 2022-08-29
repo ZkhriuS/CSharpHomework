a52c0408884d25be1768bd162f9fc6d05f9556b7
Zakhar <seriy.zachar@yandex.by>
2022-08-29 08:56:36 +0300

    final task8

---
+++ seminar1/task8/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8  */
+Console.Write("Enter a value> ");
+if (int.TryParse(Console.ReadLine(), out int first))
+{
+    int start = 1, step = 2;
+    if (first < start)
+    {
+        start--;
+        step = -2;
+    }
+    else start++;
+    System.Console.Write(" -> ");
+    while (first - start > 1 || first - start < -1)
+    {
+        Console.Write($"{start}, ");
+        start += step;
+    }
+    Console.Write($"{start} ");
+}
+else
+{
+    Console.WriteLine(" -> Incorrect input");
+}
