aeab8e9f70e378b1fb0be6e6cab02bf8f3b355d5
Zakhar <seriy.zachar@yandex.by>
2022-08-30 10:12:53 +0300

    final task 13

---
+++ seminar2/task13/Program.cs
@@ -0,0 +1,9 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6  */
+int value = new Random().Next();
+int power = 1;
+while (value / power > 999) power *= 10;
+if (value > 99) Console.WriteLine($"third digit of {value} is {value / power % 10}");
+else Console.WriteLine($"there isn't third digit in {value}");
