a32959eddf1a4aa1b5995f4f5ac8c7b800779bb9
Zakhar <seriy.zachar@yandex.by>
2022-08-31 09:40:44 +0300

    final task 25

---
+++ seminar4/task25/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16 */
+
+int Pow(int value, int power)
+{
+    int result = 1;
+    while (power > 0)
+    {
+        result *= value;
+        power--;
+    }
+    return result;
+}
+
+Console.WriteLine("Enter value and power:");
+if (int.TryParse(Console.ReadLine(), out int a) && int.TryParse(Console.ReadLine(), out int n))
+    Console.Write($"{a}^{n} = {Pow(a, n)}");
