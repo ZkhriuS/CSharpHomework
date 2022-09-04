1c8b40f5081810f94ce78b9eaaf9a0211d1e1f50
Zakhar <seriy.zachar@yandex.by>
2022-09-04 17:51:11 +0300

    final task 1

---
+++ seminar3/task1/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Задача 1: напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да  */
+string Prompt(string message)
+{
+    Console.Write(message);
+    return Console.ReadLine() ?? string.Empty;
+}
+
+bool IsPalindrome(int number)
+{
+    if (number >= 0)
+    {
+        int temp = number, reverseNumber = 0;
+        while (temp > 0)
+        {
+            reverseNumber = reverseNumber * 10 + temp % 10;
+            temp /= 10;
+        }
+        return reverseNumber == number;
+    }
+    Console.WriteLine("Number must be non-negative!");
+    return false;
+}
+
+if (int.TryParse(Prompt("Enter number > "), out int num))
+{
+    if (IsPalindrome(num))
+        Console.WriteLine($"Number {num} IS a palindrome");
+    else if (num > 0)
+        Console.WriteLine($"Number {num} is NOT a palindrome");
+}
+else Console.WriteLine("Not a number!");
