28a64851c2485e700fb9662d3ba36a8fb3a5e476
Zakhar <seriy.zachar@yandex.by>
2022-09-04 10:30:59 +0300

    final task 2

---
+++ seminar2/task2/Program.cs
@@ -0,0 +1,8 @@
+﻿int RemoveSecondIn3()
+{
+    int value = new Random().Next(100, 1000);
+    Console.Write($"{value} -> ");
+    return value / 100 * 10 + value % 10;
+}
+
+Console.WriteLine(RemoveSecondIn3());
