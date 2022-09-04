3d746f80beeaed45d6700e68d546b448111757c7
Zakhar <seriy.zachar@yandex.by>
2022-09-04 10:05:07 +0300

    renamed task15

--- seminar2/task15/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿/* Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-6 -> да
-7 -> да
-1 -> нет */
-int value = new Random().Next(1, 8);
-Console.Write(value);
-if (value < 6) Console.WriteLine(" -> no");
-else Console.WriteLine(" -> yes");
---
+++ seminar2/task4/Program.cs
@@ -0,0 +1,6 @@
+﻿/* Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую 
+день недели, и проверяет, является ли этот день выходным. */
+int value = new Random().Next(1, 8);
+Console.Write(value);
+if (value < 6) Console.WriteLine(" -> no");
+else Console.WriteLine(" -> yes");
