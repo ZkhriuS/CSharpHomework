438a38fc98d3395891feca934979fdc4bbc14104
Zakhar <seriy.zachar@yandex.by>
2022-09-04 10:19:34 +0300

    final task 4

--- seminar2/task4/Program.cs
+++ seminar2/task4/Program.cs
@@ -1,6 +1,25 @@
 ﻿/* Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую 
 день недели, и проверяет, является ли этот день выходным. */
-int value = new Random().Next(1, 8);
-Console.Write(value);
-if (value < 6) Console.WriteLine(" -> no");
-else Console.WriteLine(" -> yes");
+
+string Prompt(string message)
+{
+    Console.Write(message);
+    return Console.ReadLine() ?? string.Empty;
+}
+
+string Weekends(int value)
+{
+    if (value > 0 && value < 8)
+    {
+        Console.Write(value);
+        if (value < 6) return " -> no";
+        return " -> yes";
+    }
+    return "Not a day of the week!";
+}
+
+if (int.TryParse(Prompt("Enter day of the week > "),
+            out int day))
+    Console.WriteLine(Weekends(day));
+else
+    Console.WriteLine("Not a number!");
