64edfc7d5329e82ef1708b4e40f669f5ad4c25fe
Zakhar <seriy.zachar@yandex.by>
2022-09-04 09:47:20 +0300

    renamed task13

---
+++ seminar2/task3/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 3: Напишите программу, которая выводит третью цифру заданного числа 
+или сообщает, что третьей цифры нет. */
+
+string Prompt(string message)
+{
+    Console.Write(message);
+    return Console.ReadLine() ?? string.Empty;
+}
+
+void ThirdDigit(int value)
+{
+    int power = 1;
+    while (value / power > 999) power *= 10;
+    if (value > 99)
+        Console.WriteLine($"third digit of {value} is {value / power % 10}");
+    else
+        Console.WriteLine($"there isn't third digit in {value}");
+}
+
+if (int.TryParse(Prompt("Enter 3-digit number > "),
+            out int myValue))
+    ThirdDigit3(myValue);
+else
+    Console.WriteLine("Not a number!");
