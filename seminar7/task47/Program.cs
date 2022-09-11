9099cb6b12f1fa81a3f02ed26f3cb2a3a53dedad
Zakhar <seriy.zachar@yandex.by>
2022-09-11 21:40:27 +0300

    final task47

---
+++ seminar7/task47/Program.cs
@@ -0,0 +1,48 @@
+﻿/* Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9  */
+
+int PromptInt(string message)
+{
+    System.Console.Write(message);
+    if (int.TryParse(Console.ReadLine(), out int input))
+        return input;
+    return -1;
+}
+
+double[,] CreateArray(int rows, int columns)
+{
+    double[,] array = new double[rows, columns];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-100, 100) + new Random().NextDouble();
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]:f2}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int rows = PromptInt("Enter number of rows > ");
+int columns = PromptInt("Enter number of columns > ");
+if (rows > 0 && columns > 0)
+{
+    double[,] thisArray = CreateArray(rows, columns);
+    PrintArray(thisArray);
+}
+else
+    System.Console.WriteLine("Can't create array");
