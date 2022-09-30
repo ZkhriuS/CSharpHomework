02cb37dfdfb6d6b0a0d7925061aead0e9a8fe1ac
Zakhar <seriy.zachar@yandex.by>
2022-09-30 08:39:22 +0300

    ready seminar 8

---
+++ seminar8/task1/Program.cs
@@ -0,0 +1,73 @@
+﻿/* Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы 
+каждой строки двумерного массива.
+*/
+
+int PromptInt(string message)
+{
+    System.Console.Write(message);
+    if (int.TryParse(Console.ReadLine(), out int input))
+        return input;
+    return -1;
+}
+
+int[,] CreateArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-100, 100);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void DescInRows(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        DescInRow(array, i);
+    }
+}
+
+void DescInRow(int[,] array, int row)
+{
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        for (int j = i; j < array.GetLength(1); j++)
+        {
+            if (array[row, j] > array[row, i])
+            {
+                int temp = array[row, i];
+                array[row, i] = array[row, j];
+                array[row, j] = temp;
+            }
+        }
+    }
+}
+
+int rows = PromptInt("Enter number of rows > ");
+int columns = PromptInt("Enter number of columns > ");
+if (rows > 0 && columns > 0)
+{
+    int[,] thisArray = CreateArray(rows, columns);
+    PrintArray(thisArray);
+    DescInRows(thisArray);
+    System.Console.WriteLine();
+    PrintArray(thisArray);
+}
+else
+    System.Console.WriteLine("Can't create array");
---
+++ seminar8/task2/Program.cs
@@ -0,0 +1,63 @@
+﻿/* Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку
+с наименьшей суммой элементов. */
+
+int PromptInt(string message)
+{
+    System.Console.Write(message);
+    if (int.TryParse(Console.ReadLine(), out int input))
+        return input;
+    return -1;
+}
+
+int[,] CreateArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-100, 100);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int CountSumInRow(int[,] array, int row)
+{
+    int sum = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+        sum += array[row, i];
+    return sum;
+}
+
+int IndexMinSumRow(int[,] array)
+{
+    int minSumIndex = 0;
+    for (int i = 1; i < array.GetLength(0); i++)
+        if (CountSumInRow(array, minSumIndex) > CountSumInRow(array, i))
+            minSumIndex = i;
+    return minSumIndex;
+}
+
+int rows = PromptInt("Enter number of rows > ");
+int columns = PromptInt("Enter number of columns > ");
+if (rows > 0 && columns > 0)
+{
+    int[,] thisArray = CreateArray(rows, columns);
+    PrintArray(thisArray);
+    System.Console.WriteLine($"Row with minimum sum: {IndexMinSumRow(thisArray) + 1}");
+}
+else
+    System.Console.WriteLine("Can't create array");
